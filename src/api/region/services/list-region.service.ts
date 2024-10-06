b870d3776e4090f60cf755ec1e5d301244e0148b
Elysson Marconi <ellyssonmiike@hotmail.com>
2024-10-06 16:00:13 -0300

    fix: coordinates and central point parsing.

--- src/api/region/services/list-region.service.ts
+++ src/api/region/services/list-region.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { RegionRepository } from '@/database/repositories/region.repository';
+import { decodeCoordinates, parseCentralPoint } from '@/common/utils/location';
 
 @Injectable()
 export class ListRegionsService {
   constructor(private readonly regionRepository: RegionRepository) {}
 
   async execute() {
-    return this.regionRepository.list();
+    const data = await this.regionRepository.list();
+    return data.map(({ company, ...region }) => ({
+      id: region.id,
+      name: region.name,
+      centralPoint: parseCentralPoint(region.centralPoint),
+      coordinates: decodeCoordinates(region.coordinates),
+      company,
+    }));
   }
 }
